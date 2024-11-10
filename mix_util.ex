0b9314c84073f763ffe27cc72b8bbdb10e755dbb
red-jade <katagorikal@gmail.com>
2024-11-10 15:19:21 +0700

    Introduce the rel scope

--- mix_util.ex
+++ mix_util.ex
@@ -9,7 +9,14 @@ defmodule Exa.MixUtil do
   # local types
   # -----------
 
-  @typep scope() :: :local | :main | :tag
+  # scopes define where dependencies come from:
+  # - local: sibling repo directories in the local filesystem
+  # - main:  github main branches (but problems with caching)
+  # - tag:   explicit tagged versions specified in this file
+  # - rel:   versions written in the checked-in deps.ex file
+  #          which is over-written by the 'tag' phase
+
+  @typep scope() :: :local | :main | :tag | :rel
   @typep lib() :: atom()
   @typep dep() :: {atom(), Keyword.t()}
 
@@ -41,10 +48,17 @@ defmodule Exa.MixUtil do
     {:ex_doc, "~> 0.30", only: [:dev, :test], runtime: false},
 
     # benchmarking
-    {:benchee, "~> 1.0", only: [:dev, :test]}
+    {:benchee, "~> 1.0", only: [:dev, :test], runtime: false}
   ]
 
   @sup_libs Enum.map(@sup_deps, &elem(&1, 0))
+
+  # this umbrella project
+  @exa {:exa,
+        git: "https://github.com/red-jade/exa.git",
+        branch: "main",
+        only: [:dev, :test],
+        runtime: false}
 
   # ----------------
   # public interface
@@ -77,7 +91,7 @@ defmodule Exa.MixUtil do
     scope = arg_build()
     deps_path = deps_file(scope)
 
-    if not File.exists?(deps_path) do
+    if scope != :rel and not File.exists?(deps_path) do
       write_deps_file(scope, libs, deps_path)
     end
 
@@ -85,7 +99,7 @@ defmodule Exa.MixUtil do
       IO.puts("No exa dependency file: #{deps_path}")
       []
     else
-      deps = deps_path |> Code.eval_file() |> elem(0)
+      deps = deps_path |> Code.eval_file() |> elem(0) |> add_exa(scope)
 
       if String.starts_with?(cmd, ["deps", "compile"]) and cmd != "deps.tree" do
         IO.inspect(deps, label: "#{name} #{scope}")
@@ -95,14 +109,19 @@ defmodule Exa.MixUtil do
     end
   end
 
-  # write the 'scope'_deps.ex file for dependencies
+  # prepend this exa project if it is not a release build
+  defp add_exa(deps,:rel), do: deps
+  defp add_exa(deps,_), do: [@exa|deps]
+
+  # write the 'scope' deps.ex file for dependencies
   @spec write_deps_file(scope(), [lib()], String.t()) :: :ok
   defp write_deps_file(scope, libs, deps_path) do
     exas = Enum.filter(libs, &is_map_key(@exa_tags, &1))
     sups = Enum.filter(libs, &(&1 in @sup_libs))
     IO.puts("EXA build '#{scope}'")
-    IO.puts("EXA libraries: #{inspect(exas)}")
-    IO.puts("EXA support:   #{inspect(sups)}")
+    IO.puts("EXA libraries:  #{inspect(exas)}")
+    IO.puts("EXA support:    #{inspect(sups)}")
+    IO.puts("EXA write file: #{deps_path}")
     exa_deps = Enum.map(exas, &lib2dep(scope, &1))
     sup_deps = Enum.filter(@sup_deps, fn dep -> elem(dep, 0) in sups end)
     deps = exa_deps ++ sup_deps
@@ -129,18 +148,22 @@ defmodule Exa.MixUtil do
   defp tag(lib), do: Map.fetch!(@exa_tags, lib)
 
   # the deps literal file written by this mix task
+  # transient files are written to deps subdirectory
+  # release file is read/written to the top-level deps.ex
   @spec deps_file(scope()) :: String.t()
+  defp deps_file(:rel), do: Path.join([".", "deps.ex"])
+  defp deps_file(:tag), do: Path.join([".", "deps.ex"])
   defp deps_file(scope), do: Path.join([".", "deps", "deps_#{scope}.ex"])
 
   # parse the build scope from:
   # - mix command line --build option
   # - MIX_BUILD system environment variable
-  # - default to "tag"
+  # - default to "rel"
   @spec arg_build() :: scope()
   defp arg_build() do
     default =
       case System.fetch_env("MIX_BUILD") do
-        :error -> "tag"
+        :error -> "rel"
         {:ok, mix_build} -> mix_build
       end
 
